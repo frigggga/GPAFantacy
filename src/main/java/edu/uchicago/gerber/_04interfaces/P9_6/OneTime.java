047cfd0ed6de9db941b3fe4031efb57fa7b26939
Isha Zhang <ishz@uchicago.edu>
2024-03-27 02:39:16 -0500

    rearrange & cleaned files

--- src/main/java/edu/uchicago/gerber/_04interfaces/P9_6/OneTime.java
+++
@@ -1,8 +0,0 @@
-package edu.uchicago.gerber._04interfaces.P9_6;
-
-public class OneTime extends Appointment{
-    public OneTime(String des, int y, int m, int d){
-        super(des, y, m, d);
-    }
-
-}
--- src/main/java/edu/uchicago/gerber/_08final/GPAFantacy/model/TowerLevel.java
+++ src/main/java/edu/uchicago/gerber/_08final/GPAFantacy/model/TowerLevel.java
@@ -2,14 +2,14 @@ package edu.uchicago.gerber._08final.GPAFantacy.model;
 
 public enum TowerLevel {
 
-    LEVEL11(1, 1, 100, 30, 200, 5, "level 1"),
+    LEVEL11(1, 1, 100, 60, 200, 5, "level 1"),
     LEVEL12(2, 1, 100, 30, 150, 6, "level 2"),
     LEVEL13(3, 1, 100, 30, 100, 7, "level 3"),
 
-    LEVEL21(1, 2, 200, 50, 150, 7, "level 1"),
+    LEVEL21(1, 2, 200, 100, 150, 7, "level 1"),
     LEVEL22(2, 2, 200, 50, 50, 8, "level 2"),
 
-    LEVEL31(1, 3, 300, 80, 350, 9, "level 1"),
+    LEVEL31(1, 3, 300, 200, 350, 9, "level 1"),
     LEVEL32(2, 3, 300, 80, 150, 10, "level 2");
 
     final int curLevel;
